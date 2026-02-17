073efd2b937a4db1015d8495bf7bc3c32591b14b
Pallavi Satram <pallavisatram@gmail.com>
2026-02-17 10:54:01 +0530

    make cartQty Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,6 +72,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       quantity:1
     });
     }
-    console.log(cart);
+    let cartQty = 0;
+    cart.forEach((item) => {
+      cartQty += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQty;
   });
 });

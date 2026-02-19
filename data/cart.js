e26f83c785559e7c80b201b2acdd15be630a1fa8
Pallavi Satram <pallavisatram@gmail.com>
2026-02-19 22:07:08 +0530

    Save cart in Local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [
   {
     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2
@@ -7,6 +10,11 @@ export let cart = [
     quantity:1
   }
 ];
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -23,7 +31,9 @@ export function addToCart(productId){
       productId:productId,
       quantity:1
     });
-    }   
+    }  
+    
+    saveToStorage();
 }
 
 export function removeFromCart(productId)
@@ -36,4 +46,6 @@ export function removeFromCart(productId)
       }
     });
     cart = newCart;
+
+    saveToStorage();
 }

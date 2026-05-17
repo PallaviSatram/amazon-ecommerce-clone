3ecf6d31cdb93cc5f7d10db72fc03822b91b03be
Pallavi Satram <pallavisatram@gmail.com>
2026-05-17 17:07:34 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -70,6 +70,34 @@ logThis.call('hello');
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json() //response.json() is asynchronous, it returns a Promise.
+
+  }).then((productsData) => {
+    const newProducts = productsData.map((productDetails) => {
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }
+
+      return new Product(productDetails);
+    });
+
+    products.push(...newProducts);
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+}); */
+
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
